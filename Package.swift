645a19e407f1cbd9c9bc1eb6ca05993740925918
Aaron Roggow <aaron.roggow@notarize.com>
2022-03-17 16:21:09 -0400

    version fix

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskChatProvidersSDK",
                  url: "https://github.com/aaronroggow/chat_providers_sdk_ios",
-                 .exact("2.11.2")),
+                 .exact("2.11.3")),
         .package(name: "ZendeskMessagingSDK",
                  url: "https://github.com/aaronroggow/messaging_sdk_ios",
-                 .exact("3.8.3"))
+                 .exact("3.8.4"))
     ],
     targets: [
         .binaryTarget(
